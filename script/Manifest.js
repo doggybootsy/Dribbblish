75f63c3b6528e064caff4addfa1368a13249f76f
doggybootsy <71196819+doggybootsy@users.noreply.github.com>
2021-08-10 12:50:01 -0700

    Switched to new format

---
+++ script/Manifest.js
@@ -0,0 +1,12 @@
+module.exports = { 
+    name: "Dribbblish",
+    description: "A discord theme based off of the Dribbblish spicetify theme",
+    version: "1.0.1",
+    author: ["Doggybootsy"],
+    author_id: ["515780151791976453"],
+    theme: "./dist/compiled.css",   
+    license: "MIT",
+    source: "https://github.com/doggybootsy/Dribbblish",
+    website: "https://doggybootsy.github.io/",
+    tags: ["Dribbblish", "mini-theme"]
+};
--- script/index.js
+++ script/index.js
@@ -1,11 +1,50 @@
-const manifest = require('./theme.json');
+const manifest = require('./manifest');
 const fs = require('fs');
-function Root() {if (manifest.root) {const vars = Object.keys(manifest.root).map(e => `    --${e}: ${manifest.root[e]};`).join(`\n`);return `:root{\n${vars}\n}`}else {return ``}}
-const theme = {
-    BetterDiscord: {0: `./support/${manifest.name}.theme.css`, 1: `/**\n * @name ${manifest.name}\n * @author  ${manifest.author} \n * @version ${manifest.version} \n * @description ${manifest.description} \n * @source ${manifest.source} \n * @website ${manifest.website}\n */\n\n@import url('${manifest.website}${manifest.name}/${manifest.theme.replace('./', '')}');\n\n${Root()}`},
-    Stylus: {0: `./support/index.user.css`, 1: `@-moz-document domain("discord.com") {\n    /* ==UserStyle==\n    @name           ${manifest.name}\n    @namespace      ${manifest.source}\n    @version        ${manifest.version}\n    @description    ${manifest.description}\n    @author         ${manifest.author}\n    ==/UserStyle== */\n\n    @import url('${manifest.website}${manifest.name}/${manifest.theme.replace('./', '')}');\n\n${Root()}\n}`},
-    Powercord: {0: `./powercord_manifest.json`, 1: `{\n    "name": "${manifest.name}",\n    "description": "${manifest.description}",   \n    "version": "${manifest.version}",   \n    "author": "${manifest.author}",   \n    "theme": "${manifest.theme}",   \n    "license": "${manifest.license}"\n}`},
-    Visality: {0: `./manifest.json`, 1: `{\n    "name": "${manifest.name}",\n    "description": "${manifest.description}",   \n    "version": "${manifest.version}",   \n    "author": "${manifest.author}",   \n    "theme": "${manifest.theme}",   \n    "license": "${manifest.license}"\n}`}
+function VersionWarning(spr) {
+	if (manifest.Has_Version_warning === true) {
+		return `	${spr}/* Do not touch */\n	${spr}--version-${manifest.version.replaceAll('.', '-')}: none;`
+	} else {
+		return ''
+	}
 }
+function Root(spr) {
+	if (manifest.root && spr != undefined) {
+		const vars = Object.keys(manifest.root).map(e => `${spr}    --${e}: ${manifest.root[e]};`).join(`\n`);
+		return `${spr}:root{\n${vars}${VersionWarning('') == '' ? '' : `\n`}${VersionWarning(spr)}\n${spr}}`
+	} else {
+		if (manifest.root) {
+            const vars = Object.keys(manifest.root).map(e => `    --${e}: ${manifest.root[e]};`).join(`\n`);
+            return `:root{\n${vars}${VersionWarning('') == '' ? '' : `\n`}${VersionWarning('')}\n}`
+        }
+        else {return ''}
+	}
+}
+const meta = {
+	author: Object.keys(manifest.author).map(e => manifest.author[e]).join(' & '),
+	id: Object.keys(manifest.author_id).map(e => manifest.author_id[e]).join(' & '),
+	import: `@import url('${manifest.website}${manifest.name}/${manifest.theme.replace('./', '')}');`
+};
+const theme = {
+	BetterDiscord: {
+		0: `./support/${manifest.name}.theme.css`,
+		1: `/**\n * @name ${manifest.name}\n * @author  ${meta.author} \n * @authorId ${meta.id} \n * @version ${manifest.version} \n * @description ${manifest.description} \n * @source ${manifest.source} \n * @website ${manifest.website}\n */\n\n${meta.import}${Root() === `` ? `` : `\n\n`}${Root()}`
+	},
+	Stylus: {
+		0: `./support/index.user.css`,
+		1: `@-moz-document domain("discord.com") {\n    /* ==UserStyle==\n    @name           ${meta.author} \n    @namespace      ${manifest.source}\n    @version        ${manifest.version}\n    @description    ${manifest.description}\n    @author         ${Object.keys(manifest.author).map(e => manifest.author[e]).join(' ')} \n    ==/UserStyle== */\n\n    ${meta.import}${Root() === `` ? `` : `\n\n`}${Root("	")}\n}`
+	},
+	Powercord: {
+		0: `./powercord_manifest.json`,
+		1: `{\n    "name": "${manifest.name}",\n    "description": "${manifest.description}",   \n    "version": "${manifest.version}",   \n    "author": "${meta.author}",   \n    "theme": "${manifest.theme}",   \n    "license": "${manifest.license}"\n}`
+	},
+	Visality: {
+		0: `./manifest.json`,
+		1: `{\n    "name": "${manifest.name}",\n    "description": "${manifest.description}",   \n    "version": "${manifest.version}",   \n    "author": "${meta.author}",   \n    "theme": "${manifest.theme}",   \n    "license": "${manifest.license}"\n}`
+	}
+};
 if (!fs.existsSync(`./support`)) fs.mkdirSync(`./support`);
-Object.keys(theme).forEach(e => {fs.writeFile(theme[e][0], theme[e][1], function(err) {if(err) return console.log(err)});})
+Object.keys(theme).forEach(e => {
+	fs.writeFile(theme[e][0], theme[e][1], function(err) {
+		if (err) return console.log(err)
+	});
+})
